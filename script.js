db356bf457166bd0c627756c3a7ecc8db8fd0080
Janardhan Biharilal Yadav <63313078+janardhanyadav@users.noreply.github.com>
2024-02-21 22:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
 // your code here
+	let arr = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[num];
 }
 
 module.exports = fibonacci;
