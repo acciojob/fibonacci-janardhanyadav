1cc32a56764f1047fd800a9fa5b98cd122eb4c49
Janardhan Biharilal Yadav <63313078+janardhanyadav@users.noreply.github.com>
2024-02-21 23:05:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function fibonacci(num) {
 // your code here
-	let arr = [0, 1];
-  for (let i = 2; i <= num; i++) {
-    arr[i] = arr[i - 1] + arr[i - 2];
-  }
-  return arr[num];
+	if (num <= 0) {
+        return "Invalid input. Please provide a positive integer.";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let fibArray = [0, 1];
+        for (let i = 2; i < num; i++) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        }
+
+        return fibArray[num - 1];
 }
 
 module.exports = fibonacci;
